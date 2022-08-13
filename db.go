a1893025fd49ea37e053fc6e670be5f94fb8a90b
Jordan Walsh <jowalsh@redhat.com>
2022-08-13 11:47:18 -0400

    wip

--- db.go
+++ db.go
@@ -115,12 +115,12 @@ func (microrm *Microrm) DropTable(o interface{}) (bool, error) {
 	return true, nil
 }
 
-func (microrm *Microrm) FindWhere(tableSlice []interface{}, field string, operator string, value string) (bool, error) {
-	operatingTable := tableSlice[0]
-	tableName := inferTableName(operatingTable)
-	//use the field mapper as in the comment below.
+// func (microrm *Microrm) FindWhere(tableSlice []interface{}, field string, operator string, value string) (bool, error) {
+// 	operatingTable := tableSlice[0]
+// 	tableName := inferTableName(operatingTable)
+// 	//use the field mapper as in the comment below.
 
-}
+// }
 
 //refactor this to use field mapper function
 func (microrm *Microrm) FindById(tableObj interface{}, id int) (bool, error) {
